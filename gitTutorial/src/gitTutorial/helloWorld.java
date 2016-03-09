157bc991ebeda3fd982d76124932fe2f1a9783c8
tien n <tien.n@sbcglobal.net>
2016-03-08 22:12:11 -0800

    Change-Id: Ia964b83a0b3fd3200d53860585fde4c03210bd39

--- gitTutorial/src/gitTutorial/helloWorld.java
+++ gitTutorial/src/gitTutorial/helloWorld.java
@@ -4,7 +4,7 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello !!");
+		System.out.println("Hello Git, from branch bug#1234 !!");
 	}
 
 }
